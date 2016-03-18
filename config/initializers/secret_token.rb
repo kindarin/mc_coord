787dc93b41e6523916144870097f049ecc0e687a
Terry Ferrell <terryferrell@seizethedeal.com>
2016-03-18 10:58:04 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+McCoord::Application.config.secret_token = 'ea545ab197fbc9b995afd6bdaf96d42ab1a365784c78cb1e72518bddeba4312a80b9bfacbe78e1488bb5688ff59e555c8e08534db9f71c581bb6a0bd5fa002b4'
